b46d5d1f5d847663df3ee49c71f44c4e35198e6e
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2025-06-25 15:00:55 +0300

    feat: added NativeWind

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -1,11 +1,13 @@
 import { Text, View } from "react-native";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView style={{ flex: 1, alignSelf: "stretch" }} edges={["top"]}>
       <View style={{ flex: 1, alignSelf: "stretch", backgroundColor: "#fff" }}>
-        <Text>Welcome to the Home Screen!</Text>
+        <Text className="text-lg font-bold text-red-400 text-[40px]">
+          Welcome to the Home Screen!
+        </Text>
       </View>
     </SafeAreaView>
   );
